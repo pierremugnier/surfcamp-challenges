895fa247dda02594db55bc013f0e3e1106f814aa
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-08 20:09:23 +0200

    ex2 fin

--- Beginners/01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
+++ Beginners/01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
@@ -23,16 +23,15 @@ def is_divisible_by_two(an_integer)
 end
 
 def random_subset(an_array, sample_size)
-  # TODO: return a random subset of an_array with size sample_size
-  # example: random_subset(('a'..'z').to_a, 4) => ["u", "q", "l", "t"]
+  an_array.shuffle.sample(sample_size)
 end
 
 def randomize(an_array)
-  # TODO: return a randomized copy of an_array
-  # example: randomize([1, 2, 3, 4]) => [2, 1, 4, 3]
+  an_array.shuffle
 end
 
 def ascending_order(an_array)
   # TODO: return a copy of an_array with elements in ascending order
   # example: ascending_order([7, 3, 1, 6, 9]) => [1, 3, 6, 7, 9]
+  an_array.sort
 end
