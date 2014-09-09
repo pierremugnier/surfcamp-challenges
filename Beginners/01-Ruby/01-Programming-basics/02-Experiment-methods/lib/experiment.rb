b7292ab1eaf0d17cc14ad5047a64786893f213d8
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-09 17:15:27 +0200

    Nouvelle solution ex2

--- Beginners/01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
+++ Beginners/01-Ruby/01-Programming-basics/02-Experiment-methods/lib/experiment.rb
@@ -7,31 +7,35 @@ def get_rid_of_surrounding_whitespaces(a_string)
 end
 
 def belongs_to?(a_string, a_word)
-  a_string.include? a_word
+  return a_string.include?(a_word)
 end
+puts belongs_to?("nous sommes au surfcamp", "nous")
+puts belongs_to?("nous sommes au surfcamp", "vous")
 
 def replace(initial_string, old_letter, new_letter)
-  initial_string.gsub(old_letter, new_letter)
+  return initial_string.gsub(old_letter,new_letter)
 end
+puts replace("hello meteo","e","o")
+puts replace("hello meteo","o","e")
 
 def exactly_divide(an_integer, a_divider)
-  an_integer.to_f/a_divider
+  return an_integer.fdiv(a_divider)
 end
 
 def is_divisible_by_two(an_integer)
-  an_integer % 2 == 0
+ return an_integer.even?
 end
 
 def random_subset(an_array, sample_size)
-  an_array.shuffle.sample(sample_size)
+  return an_array.sample(sample_size)
 end
 
 def randomize(an_array)
-  an_array.shuffle
+  return an_array.shuffle
 end
+p randomize([8,9,4,17])
+
 
 def ascending_order(an_array)
-  # TODO: return a copy of an_array with elements in ascending order
-  # example: ascending_order([7, 3, 1, 6, 9]) => [1, 3, 6, 7, 9]
   an_array.sort
 end
