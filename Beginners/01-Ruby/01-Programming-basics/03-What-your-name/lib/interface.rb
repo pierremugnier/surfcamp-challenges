8e1dcc6546fb8d8fd1bd9fda929275cdd2488c71
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-09 15:56:31 +0200

    Ex 3 final

--- Beginners/01-Ruby/01-Programming-basics/03-What-your-name/lib/interface.rb
+++ Beginners/01-Ruby/01-Programming-basics/03-What-your-name/lib/interface.rb
@@ -4,6 +4,9 @@ def ask_and_get(param)
   #TODO:
   #- Ask the user to write a value for param
   #- Get and return the string inputted by the user
+  puts "Please write  a value for #{param}"
+  return gets.chomp
+
 end
 
 def name_from_terminal
@@ -19,5 +22,6 @@ full_name = name_from_terminal
 #TODO: use string concatenation to build a custom message embedding the computed full_name
 
 # custom_message =
+custom_message = "Your name is #{full_name} !"
 
 puts custom_message
