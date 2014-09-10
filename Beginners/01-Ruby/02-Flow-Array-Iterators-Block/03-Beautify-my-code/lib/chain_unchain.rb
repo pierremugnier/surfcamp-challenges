15df7a5b7f1461020ecf45c61a817dedf50ec355
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-10 11:54:05 +0200

    ex 3 no failure

--- Beginners/01-Ruby/02-Flow-Array-Iterators-Block/03-Beautify-my-code/lib/chain_unchain.rb
+++ Beginners/01-Ruby/02-Flow-Array-Iterators-Block/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,11 +1,6 @@
 def shuffle_word(a_word)
   #TODO: refactor this method
   a_word.upcase.chars.to_a.shuffle
-  #cap_word = a_word.upcase
-  #chars_enum = cap_word.chars
-  #array = chars_enum.to_a
-  #shuffle_array = array.shuffle
-
 
 end
 
@@ -13,7 +8,7 @@ end
 def quote_prime_numbers(n)
   #TODO: refactor this method
 
-  prime = (1..n).select do |i|
+  prime = (1..n).find_all do |i|
     dividers = (2..i-1).select {|k| i % k == 0 }
     dividers.empty?
   end
