c0170eb6750e47f5f5c071397d89a89f1f6b36a8
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-10 11:43:59 +0200

    ex3

--- Beginners/01-Ruby/02-Flow-Array-Iterators-Block/03-Beautify-my-code/lib/chain_unchain.rb
+++ Beginners/01-Ruby/02-Flow-Array-Iterators-Block/03-Beautify-my-code/lib/chain_unchain.rb
@@ -1,12 +1,11 @@
 def shuffle_word(a_word)
   #TODO: refactor this method
-  shuffle_array = a_word.upcase.chars.to_a.shuffle
-  cap_word = a_word.upcase
-  chars_enum = cap_word.chars
-  array = chars_enum.to_a
-  shuffle_array = array.shuffle
+  a_word.upcase.chars.to_a.shuffle
+  #cap_word = a_word.upcase
+  #chars_enum = cap_word.chars
+  #array = chars_enum.to_a
+  #shuffle_array = array.shuffle
 
-  shuffle_array
 
 end
 
@@ -14,5 +13,36 @@ end
 def quote_prime_numbers(n)
   #TODO: refactor this method
 
-  (1..n).find_all {|i| (2..i-1).select {|k| i % k == 0 }.count == 0 }.map{ |prime_num| "#{prime_num} is prime"}
+  prime = (1..n).select do |i|
+    dividers = (2..i-1).select {|k| i % k == 0 }
+    dividers.empty?
+  end
+
+  prime.map{ |prime_num| "#{prime_num} is prime"}
 end
+
+p quote_prime_numbers(30)
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  def print_name
+    puts @name
+  end
+  def grow
+    @age += 1
+  end
+  def get_ill
+    @status = "ill"
+  end
+end
+
+boris = Person.new("boris", 12)
+p boris
+boris.grow
+p boris
+boris.get_ill
+p boris
+
