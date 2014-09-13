9b266dfb5a2707279654a8ba3a25772509eef369
Pierre Mugnier <pierre.mgnr@gmail.com>
2014-09-13 19:02:30 +0200

    CSV parsing completed

--- Beginners/01-Ruby/03-Hash-Symbols/04-CSV-Parsing/lib/box_office.rb
+++ Beginners/01-Ruby/03-Hash-Symbols/04-CSV-Parsing/lib/box_office.rb
@@ -1,6 +1,25 @@
 # Encoding: utf-8
 require 'csv'
+require 'date'
 
-def most_successfull(number, max_year, file_name)
+#def most_successfull(number, max_year, file_name)
   #TODO: return the number most successfull movies max_year
+
+#end
+
+def most_successfull(nth, year, file)
+  filepath = File.dirname(__FILE__) + file
+  csv_options = {col_sep:',',quote_char:'"', encoding: "ISO8859-1"}
+
+  films = []
+  date = CSV.foreach(filepath, csv_options) do |row|
+    films << { name: row[0], year: row[1].to_i, earnings: row[2].to_i }
+  end
+
+
+  films_before_year = films.select { |film| film[:year] < year }
+  film_before_year_best_sales = films_before_year.sort_by { |film| - film[:earnings]}
+  film_most_successfull = film_before_year_best_sales.take(nth)
 end
+
+p most_successfull(4, 1996, "/movies.csv")
